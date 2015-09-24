d7bcfd6516911f8ffbbe6fc7035b4210e5d43bbd
Philipp Kroos <philipp.kroos@fh-bielefeld.de>
2015-09-24 15:28:15 +0200

    Add callback handling for vision/point-callbacks.

--- src/redapid/vision.c
+++ src/redapid/vision.c
@@ -1,102 +1,136 @@
 #include <stdlib.h>
 #include <errno.h>
 
-//#include <daemonlib/log.h>
 #include <daemonlib/event.h>
+#include <daemonlib/log.h>
 
 #include "api.h"
 #include "vision.h"
 
 #define UNUSED(x) (void) x;
-static int ok(int error_code) {
-	return error_code == API_E_SUCCESS;
+
+static LogSource _log_source = LOG_SOURCE_INITIALIZER;
+static Pipe vision_update_pipe;
+
+int vision_init(void) {
+	log_debug("Initializing vision subsystem");
+
+	if (pipe_create(&vision_update_pipe, 0) < 0) {
+		int error_code = api_get_error_code_from_errno();
+		log_error("Vision: Could not create pipe: %d",
+			  error_code);
+
+		return error_code;
+	}
+
+	return API_E_SUCCESS;
 }
 
+
 void vision_send_value_update_callback(void* object) {
-	ValueUpdate* update = (ValueUpdate*) object;
-	/*
-	log_debug("VisionDebug: Sending vision update");
-	log_error("Sending vision update");
-	*/
-	int message;
-	if (pipe_read(&update->state_change_pipe, &message, sizeof(int)) < 0) {
-		/* log_error("Could not read from state change pipe feature id %d: %s (%d)", */
-		/*	     update->feature_id, get_errno_name(errno), errno); */
+	UNUSED(object);
+
+	ValueUpdate value_update;
+
+	if (API_E_SUCCESS != pipe_read(&vision_update_pipe,
+				       &value_update, sizeof(ValueUpdate))) {
+		log_error("Could not read from pipe: %s (%d)",
+			   get_errno_name(errno), errno);
 	}
 
 	else {
-		api_send_vision_value_update_callback(update->id,
-						      update->value);
+		api_send_vision_value_update_callback(value_update.id,
+						      value_update.value);
 	}
-
-	// after the callback is sent, the update object can be removed
-	// from the event queue
-	/*
-	event_remove_source(update->state_change_pipe.read_end,
-			    EVENT_SOURCE_TYPE_GENERIC);
-	*/
-	free(update);
 }
 
 void value_callback(int8_t id, uint16_t value) {
 
-	int error_code = 0;
-	ValueUpdate* update = NULL;
-	update = calloc(1, sizeof(ValueUpdate));
+	int error_code = API_E_SUCCESS;
 
-	if (update == NULL) {
+	ValueUpdate value_update;
 
-		error_code = API_E_NO_FREE_MEMORY;
-		/* log_error("Could not allocate value-update object: %s (%d)", */
-		/*	     get_errno_name(ENOMEM), ENOMEM); */
-	}
+	value_update.id = id;
+	value_update.value = value;
 
-	else {
-		update->id = id;
-		update->value = value;
-	}
-
-	if (ok(error_code)) {
-		if (pipe_create(&update->state_change_pipe, 0) < 0) {
-			error_code = api_get_error_code_from_errno();
-			/* log_error("Could not create update for id %d: %s (%d)", */
-			/*	     update->feature_id, get_errno_name(errno), errno); */
+	if (API_E_SUCCESS == error_code) {
+		error_code = event_add_source(vision_update_pipe.read_end,
+					      EVENT_SOURCE_TYPE_GENERIC,
+					      EVENT_READ | EVENT_PRIO,
+					      vision_send_value_update_callback,
+					      NULL);
+		if (API_E_SUCCESS != error_code) {
+			log_error("Could not add update event for id %d: %d",
+				  id, error_code);
 		}
 	}
 
-	if (ok(error_code)) {
-		int error_code = event_add_source(update->state_change_pipe.read_end,
-						  EVENT_SOURCE_TYPE_GENERIC,
-						  EVENT_READ,
-						  vision_send_value_update_callback,
-						  update);
-		if (!ok(error_code)) {
-			/* log_error("Could not add update event for id %d: %s (%d)", */
-			/*	     update->feature_id, get_errno_name(errno), errno); */
+	if (API_E_SUCCESS == error_code) {
+		error_code = pipe_write(&vision_update_pipe,
+					&value_update, sizeof(ValueUpdate));
+
+		if (API_E_SUCCESS != error_code) {
+			log_error("Could not write to pipe: %d",
+				  error_code);
+
+		event_remove_source(vision_update_pipe.read_end,
+				    EVENT_SOURCE_TYPE_GENERIC);
 		}
 	}
-
-	if (ok(error_code)) {
-		int message = 1;
-		if (pipe_write(&update->state_change_pipe, &message, sizeof(int)) < 0) {
-			/* log_error("Could not write to state change pipe for feature %d: %s (%d)", */
-			/*	     feature_id, get_errno_name(errno), errno); */
-		}
-		event_remove_source(update->state_change_pipe.read_end, EVENT_SOURCE_TYPE_GENERIC);
-	}
-
-	/* log_debug("Vision: value callback add result: %d", error_code); */
-	//return error_code;
 }
 
 void vision_send_point_update_callback(void* object) {
 	UNUSED(object);
+
+	PointUpdate point_update;
+
+	if (API_E_SUCCESS != pipe_read(&vision_update_pipe,
+				       &point_update, sizeof(PointUpdate))) {
+		log_error("Could not read from pipe: %s (%d)",
+			   get_errno_name(errno), errno);
+	}
+
+	else {
+		api_send_vision_point_update_callback(point_update.id,
+						      point_update.x,
+						      point_update.y);
+	}
 }
 
 void point_callback(int8_t id, uint16_t x, uint16_t y) {
-	UNUSED(id);
-	UNUSED(x);
-	UNUSED(y);
+
+	int error_code = API_E_SUCCESS;
+
+	PointUpdate point_update;
+
+	point_update.id = id;
+	point_update.x = x;
+	point_update.y = y;
+
+	if (API_E_SUCCESS == error_code) {
+		error_code = event_add_source(vision_update_pipe.read_end,
+					      EVENT_SOURCE_TYPE_GENERIC,
+					      EVENT_READ | EVENT_PRIO,
+					      vision_send_point_update_callback,
+					      NULL);
+		if (API_E_SUCCESS != error_code) {
+			log_error("Could not add update event for id %d: %d",
+				  id, error_code);
+		}
+	}
+
+	if (API_E_SUCCESS == error_code) {
+		error_code = pipe_write(&vision_update_pipe,
+					&point_update, sizeof(PointUpdate));
+
+		if (API_E_SUCCESS != error_code) {
+			log_error("Could not write to pipe: %d",
+				  error_code);
+
+		event_remove_source(vision_update_pipe.read_end,
+				    EVENT_SOURCE_TYPE_GENERIC);
+		}
+	}
 }
 
 void vision_send_rectangle_update_callback(void* object) {
--- src/redapid/vision.h
+++ src/redapid/vision.h
@@ -6,44 +6,28 @@
 #include <daemonlib/pipe.h>
 
 typedef struct {
-	Object base;
-
 	int8_t id;
 	uint16_t value;
-
-	Pipe state_change_pipe;
 } ValueUpdate;
 
 typedef struct {
-	Object base;
-
 	int8_t id;
 	uint16_t x;
 	uint16_t y;
-
-	Pipe state_change_pipe;
 } PointUpdate;
 
 typedef struct {
-	Object base;
-
 	int8_t id;
 	uint16_t x;
 	uint16_t y;
 	uint16_t width;
 	uint16_t height;
-
-	Pipe state_change_pipe;
 } RectangleUpdate;
 
 /*
 typedef struct {
-	Object base;
-
 	int8_t id;
 	char[..] string
-
-	Pipe state_change_pipe;
 } StringUpdate;
 */
 
